ba6d122ff9c0f51b0c67365123c6689022b3869a
Arthur de Castro Martins Guaitanele <arthurguaitanele10@gmail.com>
2026-09-22 18:23:47 -0300

    Insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import entities.Department;
 import entities.Seller;
+import java.util.Date;
 import java.util.List;
 import model.DaoFactory;
 import model.SellerDao;
@@ -24,5 +25,10 @@ public class Program {
          System.out.println("\n===TESTE 3: seller findAll ===");
          list = sellerDao.findAll();
          list.forEach(System.out :: println);
+         
+         System.out.println("\n===TESTE 4: seller insert ===");
+         Seller newSeller = new Seller(null, "Greg", "Greg@gmail.com", new Date(), 1500.00, department);
+         sellerDao.insert(newSeller);
+         System.out.println("Inserted! New Id = " + newSeller.getId() );
     }
 }
